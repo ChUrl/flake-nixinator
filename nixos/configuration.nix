81d5d0dcb0f34ce12f16ad95e45ace5d832803d8
ChUrl <christoph.urlacher@protonmail.com>
2022-08-09 16:40:00 +0200

    add explicit pipewire flags

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -173,9 +173,8 @@
     pulse.enable = true;
     jack.enable = true; # We need this for low latency audio
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    # media-session.enable = true;
+    wireplumber.enable = true;
+    media-session.enable = false;
   };
 
   fonts = {
@@ -218,6 +217,7 @@
       "networkmanager"
       "wheel"
       "audio"
+      "pipewire"
       "realtime"
       "gamemode"
       "docker"
