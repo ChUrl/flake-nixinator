03916015eb8f769bef198eafc63af09d4015efd3
ChUrl <christoph.urlacher@protonmail.com>
2022-08-09 22:22:37 +0200

    add overlays/default.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,12 @@
 
 let
   # Taken from https://github.com/Misterio77/nix-config/blob/main/overlay/default.nix
+  # By specifying this we can just add our derivation to derivations/default.nix and it will land here
   additions = final: prev: import ../derivations { pkgs = final; };
 
 in
+  # TODO: I have absolutely no clue what happens here lol
+  # Basically we need some sort of list of all overlays that can be imported from the flake
+  # in the overlays = [ ... ] section of the pkgs = import nixpkgs { ... } configuration
+  # Somehow this library function turns additions into that
   nixpkgs.lib.composeManyExtensions [ additions ]
