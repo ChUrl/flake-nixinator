26cc2fca3f0801d85a9433074354d290f31189c3
ChUrl <christoph.urlacher@protonmail.com>
2022-08-13 00:33:46 +0200

    add adwaita-for-steam derivation

---
+++ derivations/adwaita-for-steam/default.nix
@@ -0,0 +1,23 @@
+{ lib, stdenv, src }:
+
+stdenv.mkDerivation {
+  pname = "adwaita-for-steam";
+  version = "unstable";
+
+  inherit src;
+
+  dontConfigure = true;
+  dontBuild = true;
+  doCheck = false;
+
+  installPhase = ''
+    mkdir -p $out/share/adwaita-for-steam
+    cp -r Adwaita/* $out/share/adwaita-for-steam
+  '';
+
+  meta = with lib; {
+    description = "A GNOME theme for Steam";
+    homepage = "https://github.com/tkashkin/Adwaita-for-Steam";
+    license = licenses.mit;
+  };
+}
