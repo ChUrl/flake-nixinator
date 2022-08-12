32d74b05002bde992b7aecf759efe65fee98078b
ChUrl <christoph.urlacher@protonmail.com>
2022-08-12 17:34:42 +0200

    reformat

--- home/christoph/nixtop/default.nix
+++ home/christoph/nixtop/default.nix
@@ -8,18 +8,18 @@ rec {
   ];
 
   modules = {
-    # TODO: Validate that this doesn't install too much
-    gaming = {
-      enable = true;
-      discordChromium.enable = true;
-    };
-
     audio = {
       enable = true;
       noisesuppression = {
         noisetorch.enable = true;
         noisetorch.autostart = true;
       };
+    };
+
+    # TODO: Validate that this doesn't install too much
+    gaming = {
+      enable = true;
+      discordChromium.enable = true;
     };
   };
 }
