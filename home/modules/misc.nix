515846593a6f23b8164a92819a05ecc0a9959c3d
ChUrl <christoph.urlacher@protonmail.com>
2022-08-15 15:09:54 +0200

    add protonmail-bridge autostart service

--- home/modules/misc.nix
+++ home/modules/misc.nix
@@ -13,33 +13,56 @@ in {
       enable = mkEnableOpt "KeePassXC";
       autostart = mkBoolOpt false "Autostart KeePassXC";
     };
+
+    protonmail = {
+      enable = mkEnableOpt "ProtonMail";
+      autostart = mkBoolOpt false "Autostart ProtonMail Bridge";
+    };
   };
 
   config = mkIf cfg.enable {
 
     home.packages = with pkgs; builtins.concatLists [
       (optionals cfg.keepass.enable [ keepassxc ])
+      (optionals cfg.protonmail.enable [ protonmail-bridge ])
     ];
 
-    systemd.user.services = {
-      autostart-keepass =
-      (mkIf (cfg.keepass.enable && cfg.keepass.autostart) {
-        Unit = {
-          Type = "oneshot";
-          Description = "KeePassXC password manager";
-          PartOf = [ "graphical-session.target" ];
-          After = [ "graphical-session.target" ];
-        };
+    systemd.user.services = mkMerge [
+      (optionalAttrs (cfg.keepass.enable && cfg.keepass.autostart) {
+        autostart-keepass = {
+          Unit = {
+            Type = "oneshot";
+            Description = "KeePassXC password manager";
+            PartOf = [ "graphical-session.target" ];
+            After = [ "graphical-session.target" ];
+          };
 
-        Service = {
-          Environment = "PATH=${config.home.profileDirectory}/bin"; # Leads to /etc/profiles/per-user/christoph/bin
-          ExecStart = "${pkgs.keepassxc}/bin/keepassxc ${config.home.homeDirectory}/Documents/KeePass/passwords.kbdx";
-#          ExecStop = "${pkgs.noisetorch}/bin/noisetorch -u";
-          Restart = "on-failure";
-        };
+          Service = {
+            # Environment = "PATH=${config.home.profileDirectory}/bin"; # Leads to /etc/profiles/per-user/christoph/bin
+            ExecStart = "${pkgs.keepassxc}/bin/keepassxc ${config.home.homeDirectory}/Documents/KeePass/passwords.kbdx";
+            # ExecStop = "${pkgs.noisetorch}/bin/noisetorch -u";
+            Restart = "on-failure";
+          };
 
-        Install.WantedBy = [ "graphical-session.target" ];
-      });
-    };
+          Install.WantedBy = [ "graphical-session.target" ];
+        };
+      })
+
+      (optionalAttrs (cfg.protonmail.enable && cfg.protonmail.autostart) {
+        autostart-protonmail = {
+          Unit = {
+            Description = "ProtonMail Bridge";
+            After = [ "network.target" ];
+          };
+
+          Service = {
+            ExecStart = "${pkgs.protonmail-bridge}/bin/protonmail-bridge --no-window --log-level info --noninteractive";
+            Restart = "always";
+          };
+
+          Install.WantedBy = [ "default.target" ];
+        };
+      })
+    ];
   };
 }
