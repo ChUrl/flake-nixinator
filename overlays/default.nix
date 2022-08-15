f1871ef3bd1f5e7ef2fe8bb0908b1a1ed747a15e
ChUrl <christoph.urlacher@protonmail.com>
2022-08-15 15:09:10 +0200

    wrap dconf-editor

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,9 +5,14 @@ let
   # By specifying this we can just add our derivation to derivations/default.nix and it will land here
   additions = final: prev: import ../derivations { inherit inputs; pkgs = final; };
 
+  modifications = final: prev: {
+    # dconf-editor-wrapped = import ./dconf-editor.nix { inherit final prev; }; # Only kept as an example
+    # Use dconf-editor.nix: { final, prev }: final.<package>.overrideAttrs (oldAttrs: { ... }) or sth similar
+  };
+
 in
   # TODO: I have absolutely no clue what happens here lol
   # Basically we need some sort of list of all overlays that can be imported from the flake
   # in the overlays = [ ... ] section of the pkgs = import nixpkgs { ... } configuration
-  # Somehow this library function turns additions into that
-  nixpkgs.lib.composeManyExtensions [ additions ]
+  # Somehow this library function turns additions/modifications into that
+  nixpkgs.lib.composeManyExtensions [ additions modifications ]
