f4c2d06c3d252457c66907f37314da35fb0ebda7
ChUrl <christoph.urlacher@protonmail.com>
2022-10-15 19:16:43 +0200

    initial (blank) plasma module

---
+++ home/modules/plasma.nix
@@ -0,0 +1,21 @@
+# TODO: KDE Connect config
+# TODO: Plasma Configuration (https://github.com/pjones/plasma-manager)
+
+{ config, lib, mylib, pkgs, ... }:
+
+with lib;
+with mylib.modules;
+
+let
+  cfg = config.modules.plasma;
+in {
+  options.modules.plasma = {
+
+  };
+
+  config = mkIf cfg.enable {
+
+    home.packages = with pkgs; [ ];
+
+  };
+}
