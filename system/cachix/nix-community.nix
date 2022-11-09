15f31e06d07e8263d4fc48e4a9e8c914bf7cfd34
ChUrl <christoph.urlacher@protonmail.com>
2022-11-09 18:30:07 +0100

    add potential nixos-rocm cachix

--- system/cachix/nix-community.nix
+++ system/cachix/nix-community.nix
@@ -3,9 +3,11 @@
   nix.settings = {
     substituters = [
       "https://nix-community.cachix.org"
+      # "https://app.cachix.org/cache/nixos-rocm"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      # "nixos-rocm.cachix.org-1:VEpsf7pRIijjd8csKjFNBGzkBqOmw8H9PRmgAq14LnE="
     ];
   };
 }
