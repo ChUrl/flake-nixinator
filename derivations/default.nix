99eeb7acf0e6e07c27f2825af0511f880f79b7bf
ChUrl <christoph.urlacher@protonmail.com>
2023-04-17 18:50:27 +0200

    Add termfilechooser portal derivation

--- derivations/default.nix
+++ derivations/default.nix
@@ -7,4 +7,5 @@
   adwaita-for-steam = pkgs.callPackage ./adwaita-for-steam {src = inputs.adwaita-for-steam;};
   dconf-editor-wrapped = pkgs.callPackage ./dconf-editor-wrapped {};
   dell-b1160w = pkgs.callPackage ./dell-b1160w {};
+  xdg-desktop-portal-termfilechooser = pkgs.callPackage ./xdg-desktop-portal-termfilechooser {};
 }
