a358e05d630f0964e1b64498658b973e9471f692
ChUrl <christoph.urlacher@protonmail.com>
2023-04-26 20:41:01 +0200

    Make hyprland config system dependent

--- home/modules/hyprland/options.nix
+++ home/modules/hyprland/options.nix
@@ -7,8 +7,23 @@ with lib;
 with mylib.modules; {
   enable = mkEnableOpt "Hyprland Window Manager + Compositor";
 
+  kb-layout = mkOption {
+    type = types.str;
+    description = "Keyboard layout to use";
+  };
+
+  kb-variant = mkOption {
+    type = types.str;
+    description = "Keyboard layout variant";
+  };
+
   theme = mkOption {
     type = types.str;
     description = "Wallpaper and colorscheme to use";
+  };
+
+  monitors = mkOption {
+    type = types.str;
+    description = "Hyprland monitor configuration";
   };
 }
