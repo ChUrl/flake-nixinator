38c53ebc215ffcfda6f24bf1ba223cdfd63aac71
ChUrl <christoph.urlacher@protonmail.com>
2023-09-08 14:04:00 +0200

    Disable waybar hyprland patches (now included natively)

--- home/modules/waybar/default.nix
+++ home/modules/waybar/default.nix
@@ -16,37 +16,38 @@ in {
 
   config = let
     # Taken from https://github.com/Ruixi-rebirth/flakes/blob/main/modules/programs/wayland/waybar/workspace-patch.nix
-    hyprctl = "${pkgs.hyprland}/bin/hyprctl";
-    workspaces-patch = pkgs.writeTextFile {
-      name = "waybar-hyprctl.diff";
-      text = ''
-        diff --git a/src/modules/wlr/workspace_manager.cpp b/src/modules/wlr/workspace_manager.cpp
-        index 6a496e6..a689be0 100644
-        --- a/src/modules/wlr/workspace_manager.cpp
-        +++ b/src/modules/wlr/workspace_manager.cpp
-        @@ -511,7 +511,9 @@ auto Workspace::handle_clicked(GdkEventButton *bt) -> bool {
-           if (action.empty())
-             return true;
-           else if (action == "activate") {
-        -    zext_workspace_handle_v1_activate(workspace_handle_);
-        +    // zext_workspace_handle_v1_activate(workspace_handle_);
-        +    const std::string command = "${hyprctl} dispatch workspace " + name_;
-        +    system(command.c_str());
-           } else if (action == "close") {
-             zext_workspace_handle_v1_remove(workspace_handle_);
-           } else {
-      '';
-    };
+    # hyprctl = "${pkgs.hyprland}/bin/hyprctl";
+    # workspaces-patch = pkgs.writeTextFile {
+    #   name = "waybar-hyprctl.diff";
+    #   text = ''
+    #     diff --git a/src/modules/wlr/workspace_manager.cpp b/src/modules/wlr/workspace_manager.cpp
+    #     index 6a496e6..a689be0 100644
+    #     --- a/src/modules/wlr/workspace_manager.cpp
+    #     +++ b/src/modules/wlr/workspace_manager.cpp
+    #     @@ -511,7 +511,9 @@ auto Workspace::handle_clicked(GdkEventButton *bt) -> bool {
+    #        if (action.empty())
+    #          return true;
+    #        else if (action == "activate") {
+    #     -    zext_workspace_handle_v1_activate(workspace_handle_);
+    #     +    // zext_workspace_handle_v1_activate(workspace_handle_);
+    #     +    const std::string command = "${hyprctl} dispatch workspace " + name_;
+    #     +    system(command.c_str());
+    #        } else if (action == "close") {
+    #          zext_workspace_handle_v1_remove(workspace_handle_);
+    #        } else {
+    #   '';
+    # };
 
-    waybar-hyprland = pkgs.waybar.overrideAttrs (oldAttrs: {
-      mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
-      patches = (oldAttrs.patches or []) ++ [workspaces-patch];
-    });
+    # waybar-hyprland = pkgs.waybar.overrideAttrs (oldAttrs: {
+    #   mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+    #   patches = (oldAttrs.patches or []) ++ [workspaces-patch];
+    # });
   in
     mkIf cfg.enable {
       programs.waybar = {
         enable = true;
-        package = waybar-hyprland;
+        # package = waybar-hyprland;
+        package = pkgs.waybar;
 
         systemd = {
           enable = false; # Gets started by hyprland
@@ -60,7 +61,7 @@ in {
           trap "${pkgs.procps}/bin/pkill waybar" EXIT
 
           while true; do
-              ${waybar-hyprland}/bin/waybar -c $HOME/NixFlake/config/waybar/config.json -s $HOME/NixFlake/config/waybar/style.css &
+              ${pkgs.waybar}/bin/waybar -c $HOME/NixFlake/config/waybar/config.json -s $HOME/NixFlake/config/waybar/style.css &
               ${pkgs.inotifyTools}/bin/inotifywait -e create,modify $HOME/NixFlake/config/waybar/config.json $HOME/NixFlake/config/waybar/style.css
               ${pkgs.procps}/bin/pkill waybar
           done
