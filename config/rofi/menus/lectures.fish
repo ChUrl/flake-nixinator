a29df0e063d4f884f1fbd4f4f85bf8c316afccab
ChUrl <christoph.urlacher@protonmail.com>
2023-10-15 11:57:28 +0200

    exa->eza

--- config/rofi/menus/lectures.fish
+++ config/rofi/menus/lectures.fish
@@ -1,13 +1,13 @@
 #!/usr/bin/env fish
 
 # User chooses lecture
-set LECTURE (exa -1 -D ~/Notes/TU | rofi -dmenu -p " lecture " -i)
+set LECTURE (eza -1 -D ~/Notes/TU | rofi -dmenu -p " lecture " -i)
 if test -z $LECTURE
     exit
 end
 
 # User chooses slides
-set DECK (exa -1 ~/Notes/TU/$LECTURE/Lecture | grep ".pdf" | rofi -dmenu -p " deck " -i)
+set DECK (eza -1 ~/Notes/TU/$LECTURE/Lecture | grep ".pdf" | rofi -dmenu -p " deck " -i)
 if test -z $DECK
     exit
 end
