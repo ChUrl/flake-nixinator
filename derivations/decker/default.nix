e4647545c2fbaa09aa6c3bf8ce3b00b040f2b94c
ChUrl <christoph.urlacher@protonmail.com>
2023-10-15 15:22:25 +0200

    Update decker derivation

--- derivations/decker/default.nix
+++ derivations/decker/default.nix
@@ -2,52 +2,63 @@
   lib,
   stdenv,
   fetchurl,
+  fetchFromGitHub,
   autoPatchelfHook,
   zlib,
   bzip2,
   ncurses,
   gmp
-}:
-stdenv.mkDerivation rec {
-  pname = "decker";
-  version = "0.13.4";
-
-  src = fetchurl {
-    url = "https://github.com/decker-edu/decker/releases/download/v0.13.4/${pname}-v${version}-Linux-X64";
-    sha256 = "sha256-LZ0j2X0zCP9XsWglc488nL25w3VmWh/TYxK1x6K6yOI=";
+}: let
+  resources = fetchFromGitHub {
+    owner = "decker-edu";
+    repo = "decker";
+    rev = "f3a2c918514d59ca611490901926bd078823b93d";
+    sha256 = "sha256-0gTg9TJ06YCF0lJx9+nVa50+GhmPsvgKIeM8DQr7G9I=";
   };
+in
+  stdenv.mkDerivation rec {
+    pname = "decker";
+    version = "0.13.4";
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-  ];
+    src = fetchurl {
+      url = "https://github.com/decker-edu/decker/releases/download/v0.13.4/${pname}-v${version}-Linux-X64";
+      sha256 = "sha256-LZ0j2X0zCP9XsWglc488nL25w3VmWh/TYxK1x6K6yOI=";
+    };
 
-  unpackCmd = ''
-    mkdir -p root
-    cp $curSrc root/decker
-  '';
+    nativeBuildInputs = [
+      autoPatchelfHook
+    ];
 
-  dontBuild = true;
+    unpackCmd = ''
+      mkdir -p root
+      cp $curSrc root/decker
+    '';
 
-  buildInputs = [
-    stdenv.cc.cc.lib
-    zlib
-    bzip2
-    ncurses
-    gmp
-  ];
+    dontBuild = true;
 
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    cp decker $out/bin/decker
-    chmod +x $out/bin/decker
-    runHook postInstall
-  '';
+    buildInputs = [
+      stdenv.cc.cc.lib
+      zlib
+      bzip2
+      ncurses
+      gmp
+    ];
 
-  meta = with lib; {
-    description = "A tool to create interactive web-based presentations.";
-    homepage = "https://decker.cs.tu-dortmund.de";
-    license = licenses.gpl3Only;
-    platforms = ["x86_64-linux"];
-  };
-}
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/bin
+      cp decker $out/bin/decker
+      cp -r ${resources}/resource $out/bin/resource
+      chmod +x $out/bin/decker
+
+      runHook postInstall
+    '';
+
+    meta = with lib; {
+      description = "A tool to create interactive web-based presentations.";
+      homepage = "https://github.com/decker-edu/decker";
+      license = licenses.gpl3Only;
+      platforms = ["x86_64-linux"];
+    };
+  }
