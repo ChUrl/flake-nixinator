fd494fafc556dd6bb748cea4a09a093861993a4a
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-06-02 04:23:10 +0200

    Use relative path for nvim config bootstrap

--- config/neovim/init.lua
+++ config/neovim/init.lua
@@ -1,2 +1,11 @@
+local addRelPath = function(dir)
+	local spath = debug.getinfo(1, "S").source:sub(2):gsub("^([^/])", "./%1"):gsub("[^/]*$", "")
+	dir = dir and (dir .. "/") or ""
+	spath = spath .. dir
+	package.path = spath .. "?.lua;" .. spath .. "?/init.lua" .. package.path
+end
+
+addRelPath()
+
 require("bootstrap")
 require("config")
