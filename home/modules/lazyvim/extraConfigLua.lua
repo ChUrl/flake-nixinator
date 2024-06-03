31e70b4973e0e4856c3009af4e5aa2c410d02970
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-06-03 20:36:27 +0200

    Update lazyvim module

--- home/modules/lazyvim/extraConfigLua.lua
+++ home/modules/lazyvim/extraConfigLua.lua
@@ -1,31 +1,5 @@
--- Package manager setup
-require("lazy").setup({
-	defaults = {
-		lazy = true,
-	},
-
-	dev = {
-		-- reuse files from pkgs.vimPlugins.*
-		path = "@lazyPath@", -- NOTE: Will be replaced by the nix path
-		patterns = { "." },
-		-- fallback to download
-		fallback = true,
-	},
-
-	spec = {
-		-- { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-		-- The following configs are needed for fixing lazyvim on nix
-		-- force enable telescope-fzf-native.nvim
-		{ "nvim-telescope/telescope-fzf-native.nvim", enabled = true },
-		-- disable mason.nvim, use config.extraPackages
-		{ "williamboman/mason-lspconfig.nvim", enabled = false },
-		{ "williamboman/mason.nvim", enabled = false },
-		-- uncomment to import/override with your plugins
-		-- { import = "plugins" },
-		-- put this line at the end of spec to clear ensure_installed
-		{ "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = {} } },
-	},
-})
+-- Skip loading LazyVim options
+package.loaded["lazyvim.config.options"] = true
 
 -- Hide inline diagnostics and show border
 vim.diagnostic.config({
@@ -39,3 +13,46 @@ vim.api.nvim_set_keymap("c", "<Up>", 'v:lua.get_wildmenu_key("<left>", "<up>")',
 function _G.get_wildmenu_key(key_wildmenu, key_regular)
 	return vim.fn.wildmenumode() ~= 0 and key_wildmenu or key_regular
 end
+
+-- Package manager setup
+require("lazy").setup({
+	defaults = {
+		lazy = true,
+	},
+
+	dev = {
+		-- reuse files from pkgs.vimPlugins.*
+		path = "@lazyPath@", -- NOTE: Will be replaced by the nix path
+
+		patterns = { "." },
+
+		-- fallback to download
+		fallback = true,
+	},
+
+	spec = {
+		{
+			"LazyVim/LazyVim",
+			import = "lazyvim.plugins",
+			opts = {
+				defaults = {
+					autocmds = false, -- Skip loading LazyVim cmds
+					keymaps = false, -- Skip loading LazyVim keymaps
+				},
+			},
+		},
+
+		-- force enable telescope-fzf-native.nvim on nix
+		{ "nvim-telescope/telescope-fzf-native.nvim", enabled = true },
+
+		-- disable mason.nvim, use config.extraPackages on nix
+		{ "williamboman/mason-lspconfig.nvim", enabled = false },
+		{ "williamboman/mason.nvim", enabled = false },
+
+		-- uncomment to import/override with your plugins
+		-- { import = "plugins" },
+
+		-- put this line at the end of spec to clear ensure_installed on nix
+		{ "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = {} } },
+	},
+})
