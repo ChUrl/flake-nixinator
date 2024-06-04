a196102ebbd2703694307dc37de57b2699c2fdcc
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-06-04 22:27:59 +0200

    Nixos: Disable old derivations

--- derivations/default.nix
+++ derivations/default.nix
@@ -5,13 +5,13 @@
   ...
 }: {
   # vital-synth = pkgs.callPackage ./vital-synth {}; # Kept as an example, don't know if I will fix this or keep using distrho
-  adwaita-for-steam = pkgs.callPackage ./adwaita-for-steam {src = inputs.adwaita-for-steam;};
-  dconf-editor-wrapped = pkgs.callPackage ./dconf-editor-wrapped {};
+  # adwaita-for-steam = pkgs.callPackage ./adwaita-for-steam {src = inputs.adwaita-for-steam;};
+  # dconf-editor-wrapped = pkgs.callPackage ./dconf-editor-wrapped {};
   dell-b1160w = pkgs.callPackage ./dell-b1160w {};
   firefox-gnome-theme = pkgs.callPackage ./firefox-gnome-theme {src = inputs.firefox-gnome-theme;};
   # modules-options-doc = pkgs.callPackage ./modules-options-doc {mylib = mylib;}; # TODO: Borked
   # spotdl-4_1_6 = pkgs.callPackage ./spotdl-4_1_6 {}; # TODO: Old
   xdg-desktop-portal-termfilechooser = pkgs.callPackage ./xdg-desktop-portal-termfilechooser {};
-  wiiu-downloader = pkgs.callPackage ./wiiu-downloader {};
+  # wiiu-downloader = pkgs.callPackage ./wiiu-downloader {};
   decker = pkgs.callPackage ./decker {};
 }
