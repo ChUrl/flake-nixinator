edaa8588b6eb0beeb37f79ab16eae7c88fa4e433
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-09-22 20:37:47 +0200

    Nixos: Update options soon to be deprecated

--- system/nixinator/default.nix
+++ system/nixinator/default.nix
@@ -101,18 +101,23 @@
     # videoDrivers = ["amdgpu"];
   };
 
-  # NOTE: This has been relocated here from the default config, because it forces en-US keyboard layout
+  # NOTE: This has been relocated here from the default config, because it forces en-US keyboard layout.
   #       The laptop needs de-DE...
   # Chinese Input
-  i18n.inputMethod.enabled = "fcitx5";
-  i18n.inputMethod.fcitx5 = {
-    waylandFrontend = true;
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
 
-    addons = with pkgs; [
-      fcitx5-gtk
-      libsForQt5.fcitx5-qt
-      fcitx5-chinese-addons
-      fcitx5-configtool # TODO: Remove this and set config through HomeManager
-    ];
+    fcitx5 = {
+      waylandFrontend = true;
+
+      addons = with pkgs; [
+        fcitx5-gtk
+        libsForQt5.fcitx5-qt # QT5
+        kdePackages.fcitx5-qt # QT6
+        fcitx5-chinese-addons
+        fcitx5-configtool # TODO: Remove this and set config through HomeManager
+      ];
+    };
   };
 }
