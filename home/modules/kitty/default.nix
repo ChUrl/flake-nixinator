5f032e475e9b52e4835cb02dc175a400c47106a4
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-10-13 17:13:11 +0200

    Kitty: Use colors from color HM module

--- home/modules/kitty/default.nix
+++ home/modules/kitty/default.nix
@@ -1,15 +1,14 @@
 # TODO: Expose some settings
 {
   config,
-  nixosConfig,
   lib,
   mylib,
-  pkgs,
   ...
 }:
 with lib;
 with mylib.modules; let
   cfg = config.modules.kitty;
+  color = config.modules.color;
   # cfgnv = config.modules.neovim;
 in {
   options.modules.kitty = import ./options.nix {inherit lib mylib;};
@@ -20,8 +19,7 @@ in {
       shellIntegration.enableFishIntegration = true;
 
       font = {
-        # package = pkgs.victor-mono;
-        name = "JetBrainsMono Nerd Font Mono";
+        name = "${color.font}";
         size = 12;
       };
 
@@ -32,9 +30,9 @@ in {
       };
 
       settings = {
-        editor = "hx";
+        editor = config.home.sessionVariables.EDITOR;
         scrollback_lines = 10000;
-        window_padding_width = 10; # Looks stupid with helix if bg doesn't match
+        window_padding_width = 10; # Looks stupid with editors if bg doesn't match
         # hide_window_decorations = "yes";
         enabled_layouts = "grid,vertical,horizontal";
 
@@ -44,82 +42,82 @@ in {
         tab_bar_min_tabs = 1;
         tab_bar_edge = "bottom";
         tab_bar_style = "powerline";
-        tab_powerline_style = "slanted";
+        tab_powerline_style = "round";
         tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
 
         #
-        # Catppuccin Light Theme
+        # Color Theme
         #
 
         # The basic colors
-        foreground = "#4C4F69";
-        background = "#EFF1F5";
-        selection_foreground = "#EFF1F5";
-        selection_background = "#DC8A78";
+        foreground = "#${color.light.text}";
+        background = "#${color.light.base}";
+        selection_foreground = "#${color.light.base}";
+        selection_background = "#${color.light.rosewater}";
 
         # Cursor colors
-        cursor = "#DC8A78";
-        cursor_text_color = "#EFF1F5";
+        cursor = "#${color.light.rosewater}";
+        cursor_text_color = "#${color.light.base}";
 
         # URL underline color when hovering with mouse
-        url_color = "#DC8A78";
+        url_color = "#${color.light.rosewater}";
 
         # Kitty window border colors
-        active_border_color = "#7287FD";
-        inactive_border_color = "#9CA0B0";
-        bell_border_color = "#DF8E1D";
+        active_border_color = "#${color.light.lavender}";
+        inactive_border_color = "#${color.light.overlay0}";
+        bell_border_color = "#${color.light.yellow}";
 
         # OS Window titlebar colors
         wayland_titlebar_color = "system";
         macos_titlebar_color = "system";
 
         # Tab bar colors
-        active_tab_foreground = "#EFF1F5";
-        active_tab_background = "#8839EF";
-        inactive_tab_foreground = "#4C4F69";
-        inactive_tab_background = "#9CA0B0";
-        tab_bar_background = "#BCC0CC";
+        active_tab_foreground = "#${color.light.base}";
+        active_tab_background = "#${color.light.mauve}";
+        inactive_tab_foreground = "#${color.light.text}";
+        inactive_tab_background = "#${color.light.overlay0}";
+        tab_bar_background = "#${color.light.surface1}";
 
-        # Colors for marks (marked text in the terminal)
-        mark1_foreground = "#EFF1F5";
-        mark1_background = "#7287fD";
-        mark2_foreground = "#EFF1F5";
-        mark2_background = "#8839EF";
-        mark3_foreground = "#EFF1F5";
-        mark3_background = "#209FB5";
+        # Color for marks (marked text in the terminal)
+        mark1_foreground = "#${color.light.base}";
+        mark1_background = "#${color.light.lavender}";
+        mark2_foreground = "#${color.light.base}";
+        mark2_background = "#${color.light.mauve}";
+        mark3_foreground = "#${color.light.base}";
+        mark3_background = "#${color.light.sapphire}";
 
         # The 16 terminal colors
         # black
-        color0 = "#5C5F77";
-        color8 = "#6C6F85";
+        color0 = "#${color.light.subtext1}";
+        color8 = "#${color.light.subtext0}";
 
         # red
-        color1 = "#D20F39";
-        color9 = "#D20F39";
+        color1 = "#${color.light.red}";
+        color9 = "#${color.light.red}";
 
         # green
-        color2 = "#40A02B";
-        color10 = "#40A02B";
+        color2 = "#${color.light.green}";
+        color10 = "#${color.light.green}";
 
         # yellow
-        color3 = "#DF8E1D";
-        color11 = "#DF8E1D";
+        color3 = "#${color.light.yellow}";
+        color11 = "#${color.light.yellow}";
 
         # blue
-        color4 = "#1E66F5";
-        color12 = "#1E66F5";
+        color4 = "#${color.light.blue}";
+        color12 = "#${color.light.blue}";
 
         # magenta
-        color5 = "#EA76CB";
-        color13 = "#EA76CB";
+        color5 = "#${color.light.pink}";
+        color13 = "#${color.light.pink}";
 
         # cyan
-        color6 = "#179299";
-        color14 = "#179299";
+        color6 = "#${color.light.teal}";
+        color14 = "#${color.light.teal}";
 
         # white
-        color7 = "#ACB0BE";
-        color15 = "#BCC0CC";
+        color7 = "#${color.light.surface2}";
+        color15 = "#${color.light.surface1}";
       };
     };
   };
