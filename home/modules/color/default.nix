94d2ff1ebbde5bcc14e6ef2fbbe570a53599d158
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-10-13 16:07:26 +0200

    Color: Add catppuccin latte and mocha schemes

--- home/modules/color/default.nix
+++ home/modules/color/default.nix
@@ -7,8 +7,60 @@
 with lib;
 with mylib.modules; let
   cfg = config.modules.color;
-in {
-  options.modules.color = import ./options.nix {inherit lib mylib;};
 
-  config = {};
+  # Options and assignments will be generated from those keys
+  colorKeys = [
+    # Colors
+    "rosewater"
+    "flamingo"
+    "pink"
+    "mauve"
+    "red"
+    "maroon"
+    "peach"
+    "yellow"
+    "green"
+    "teal"
+    "sky"
+    "sapphire"
+    "blue"
+    "lavender"
+
+    # 50 shades of gray
+    "text"
+    "subtext1"
+    "subtext0"
+    "overlay2"
+    "overlay1"
+    "overlay0"
+    "surface2"
+    "surface1"
+    "surface0"
+    "base"
+    "mantle"
+    "crust"
+  ];
+in {
+  options.modules.color = import ./options.nix {inherit lib mylib colorKeys;};
+
+  config = let
+    lightDefs = import ./${cfg.lightScheme}.nix;
+    darkDefs = import ./${cfg.darkScheme}.nix;
+
+    mkLightColorAssignment = key: {${key} = lightDefs.${key};};
+    mkDarkColorAssignment = key: {${key} = darkDefs.${key};};
+  in
+    mkIf cfg.enable {
+      # This module sets its own options
+      # to the values specified in a colorscheme file.
+      modules.color.light = lib.pipe colorKeys [
+        (builtins.map mkLightColorAssignment)
+        lib.mergeAttrsList
+      ];
+
+      modules.color.dark = lib.pipe colorKeys [
+        (builtins.map mkDarkColorAssignment)
+        lib.mergeAttrsList
+      ];
+    };
 }
--- home/modules/color/options.nix
+++ home/modules/color/options.nix
@@ -1,13 +1,58 @@
 {
   lib,
   mylib,
+  colorKeys,
   ...
 }:
 with lib;
-with mylib.modules; {
-  scheme = mkOption {
-    type = types.str;
-    description = "The color scheme to use";
-    example = "catppuccin-latte";
+with mylib.modules; let
+  mkColorOption = key: {
+    light.${key} = mkOption {
+      type = types.str;
+      description = "The RGB hex color value for ${key} in the light scheme";
+      example = "EEEEEE";
+    };
+    dark.${key} = mkOption {
+      type = types.str;
+      description = "The RGB hex color value for ${key} in the dark scheme";
+      example = "111111";
+    };
   };
-}
+in
+  (lib.pipe colorKeys [
+    (builtins.map mkColorOption)
+    lib.mergeAttrsList
+  ])
+  // {
+    enable = mkEnableOption "Enable color schemes";
+
+    lightScheme = mkOption {
+      type = types.str;
+      description = "The color scheme to use for light colors";
+      example = "catppuccin-latte";
+      default = "catppuccin-latte";
+    };
+
+    darkScheme = mkOption {
+      type = types.str;
+      description = "The color scheme to use for dark colors";
+      example = "catppuccin-mocha";
+      default = "catppuccin-mocha";
+    };
+
+    keys = mkOption {
+      type = types.listOf types.str;
+      description = "The names of all possible colors";
+      default = colorKeys;
+    };
+
+    light = mkOption {
+      type = types.attrs;
+      description = "Colors belonging to the selected light scheme";
+    };
+
+    dark = mkOption {
+      type = types.attrs;
+      description = "Colors belonging to the selected dark scheme";
+    };
+  }
