a873c000099eef51ed776efb8205db7c6253a1e9
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-12-01 14:28:31 +0100

    Neovim: Update blink-cmp mapping config

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -388,19 +388,7 @@ in {
 
             opts = {
               keymap = {
-                show = "<C-space>";
-                hide = "<C-e>";
-                accept = "<cr>";
-                select_prev = ["<Up>" "<C-p>"];
-                select_next = ["<Down>" "<C-n>"];
-
-                show_documentation = "<C-space>";
-                hide_documentation = "<C-space>";
-                scroll_documentation_up = "<C-b>";
-                scroll_documentation_down = "<C-f>";
-
-                snippet_forward = "<Tab>";
-                snippet_backward = "<S-Tab>";
+                preset = "enter";
               };
 
               accept = {
