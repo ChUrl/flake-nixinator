324fd1dc19bb9d4c33fb8884116fdfe1cd3d2186
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-03-05 15:44:53 +0100

    Obsidian: Add center_image css snippet

--- home/modules/latex/default.nix
+++ home/modules/latex/default.nix
@@ -17,14 +17,17 @@ in {
       ];
 
       file = {
-        # Old collection of macros and environments I once used, but not anymore...
+        # Collection of macros and environments I once used, but not anymore...
         # "texmf/tex/latex/custom/christex.sty".source = ../../../config/latex/christex.sty;
-        # "Notes/Obsidian/Chriphost/christex.sty".source = ../../../config/latex/christex.sty; # For obsidian notes
+        "Notes/Obsidian/Chriphost/christex.sty".source = ../../../config/latex/christex.sty; # For old obsidian notes
 
         ".indentconfig.yaml".source = ../../../config/latex/.indentconfig.yaml;
         ".indentsettings.yaml".source = ../../../config/latex/.indentsettings.yaml;
+
+        # TODO: These don't belong into a latex module
         "Notes/Obsidian/Chriphost/latex_snippets.json".source = ../../../config/obsidian/latex_snippets.json; # TODO: Symlink
         "Notes/Obsidian/Chriphost/.obsidian/snippets/latex_preview.css".source = ../../../config/obsidian/css_snippets/latex_preview.css;
+        "Notes/Obsidian/Chriphost/.obsidian/snippets/center_image.css".source = ../../../config/obsidian/css_snippets/center_image.css;
       };
     };
   };
