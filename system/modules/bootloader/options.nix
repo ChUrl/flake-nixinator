f45976621eebc71300a4bda374bbc6acb7461ea6
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-08 21:35:37 +0200

    Modules: Add bootloader module

---
+++ system/modules/bootloader/options.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  mylib,
+  ...
+}: {
+  enable = lib.mkEnableOption "Enable boot loader configuration";
+
+  loader = lib.mkOption {
+    type = lib.types.enum [
+      "grub"
+      "systemd-boot"
+    ];
+    description = "What boot loader to use";
+    example = "systemd-boot";
+    default = "systemd-boot";
+  };
+
+  systemd-boot.bootDevice = lib.mkOption {
+    type = lib.types.str;
+    description = "The path to the boot partition";
+    example = "/boot/efi";
+    default = "/boot/efi";
+  };
+
+  grub.bootDevice = lib.mkOption {
+    type = lib.types.str;
+    description = "The path to the boot partition";
+    example = "/dev/sda";
+    default = "/dev/sda";
+  };
+}
--- system/modules/default.nix
+++ system/modules/default.nix
@@ -1,5 +1,6 @@
 {...}: {
   imports = [
+    ./bootloader
     ./desktopportal
     ./fonts
     ./mime
