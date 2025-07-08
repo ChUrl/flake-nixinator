dfab92e0de40823cc187b316a25366498139e6bc
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-08 22:48:34 +0200

    System/Nixinator: Remove obsolete stuff from hardware-configuration.nix

--- system/nixinator/hardware-configuration.nix
+++ system/nixinator/hardware-configuration.nix
@@ -99,24 +99,6 @@
     }
   ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-
-  networking.useDHCP = lib.mkDefault false; # NOTE: Set to false bc systemd-networkd
-  networking.enableIPv6 = lib.mkDefault false; # NOTE: Could be overwritten by systemd-networkd
-
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0u2.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
-
-  # networking.networkmanager.enable = true;
-  # networking.wireless.iwd.enable = true;
-  # networking.networkmanager.wifi.backend = "iwd";
-
-  # nixpkgs.config.allowUnfree = true;
-
   hardware = {
     # Use all redistributable firmware (i.e. nonfree)
     enableAllFirmware = true;
