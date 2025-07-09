a499bbb814856f6dec43415e14aaae918b1aff05
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-09 18:41:03 +0200

    Modules/Sops: Allow setting secrets with "neededForUsers = true;"

--- system/modules/sops-nix/options.nix
+++ system/modules/sops-nix/options.nix
@@ -13,4 +13,15 @@
     '';
     default = [];
   };
+
+  bootSecrets = lib.mkOption {
+    type = lib.types.attrsOf (lib.types.listOf lib.types.str);
+    description = "The secrets to expose on this host earlier in the boot process";
+    example = ''
+      christoph = [
+        "user-password"
+      ];
+    '';
+    default = [];
+  };
 }
