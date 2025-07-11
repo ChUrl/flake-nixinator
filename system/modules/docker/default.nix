a8b5958ad6fe39b3e3a2c88c83f7bcfd8bb94748
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-11 16:13:30 +0200

    Modules/Docker: Fix typo

--- system/modules/docker/default.nix
+++ system/modules/docker/default.nix
@@ -10,7 +10,7 @@ in {
 
   config = lib.mkIf docker.enable {
     environment.variables = lib.mkMerge [
-      (lib.mkIf ((!docker.podman) && docker.buildkit) {
+      (lib.mkIf ((!docker.podman) && docker.docker.buildkit) {
         DOCKER_BUILDKIT = 1;
       })
     ];
