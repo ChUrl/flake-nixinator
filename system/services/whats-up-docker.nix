4ad58e20568c6922591c9d5794e7d7821bdce912
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-11 19:57:13 +0200

    Services/Wud: Update to v8.1.1

--- system/services/whats-up-docker.nix
+++ system/services/whats-up-docker.nix
@@ -3,36 +3,40 @@
   lib,
   pkgs,
   ...
-}: {
-  virtualisation.oci-containers.containers.whats-up-docker = {
-    image = "getwud/wud:latest";
-    autoStart = true;
+}: let
+  wudVersion = "8.1.1";
+in {
+  virtualisation.oci-containers.containers = {
+    whats-up-docker = {
+      image = "getwud/wud:${wudVersion}";
+      autoStart = true;
 
-    login = {
-      # Uses DockerHub by default
-      # registry = "";
+      login = {
+        # Uses DockerHub by default
+        # registry = "";
 
-      # DockerHub Credentials
-      username = "christoph.urlacher@protonmail.com";
-      passwordFile = "${config.sops.secrets.docker-password.path}";
+        # DockerHub Credentials
+        username = "christoph.urlacher@protonmail.com";
+        passwordFile = "${config.sops.secrets.docker-password.path}";
+      };
+
+      dependsOn = [
+        # "pihole"
+      ];
+
+      ports = [
+        # "3001:3000"
+      ];
+
+      volumes = [
+        "/var/run/docker.sock:/var/run/docker.sock"
+      ];
+
+      environment = {};
+
+      extraOptions = [
+        "--net=behind-nginx"
+      ];
     };
-
-    dependsOn = [
-      # "pihole"
-    ];
-
-    ports = [
-      # "3001:3000"
-    ];
-
-    volumes = [
-      "/var/run/docker.sock:/var/run/docker.sock"
-    ];
-
-    environment = {};
-
-    extraOptions = [
-      "--net=behind-nginx"
-    ];
   };
 }
