cabc48164f7752f503b50fdfe5fb6e8a51f8a061
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-11 21:37:12 +0200

    Services/Jellyfin: Update to v10.10.7

--- system/services/jellyfin.nix
+++ system/services/jellyfin.nix
@@ -3,46 +3,50 @@
   lib,
   pkgs,
   ...
-}: {
-  virtualisation.oci-containers.containers.jellyfin = {
-    image = "linuxserver/jellyfin:latest";
-    autoStart = true;
+}: let
+  jellyfinVersion = "10.10.7";
+in {
+  virtualisation.oci-containers.containers = {
+    jellyfin = {
+      image = "linuxserver/jellyfin:${jellyfinVersion}";
+      autoStart = true;
 
-    login = {
-      # Uses DockerHub by default
-      # registry = "";
+      login = {
+        # Uses DockerHub by default
+        # registry = "";
 
-      # DockerHub Credentials
-      username = "christoph.urlacher@protonmail.com";
-      passwordFile = "${config.sops.secrets.docker-password.path}";
+        # DockerHub Credentials
+        username = "christoph.urlacher@protonmail.com";
+        passwordFile = "${config.sops.secrets.docker-password.path}";
+      };
+
+      dependsOn = [
+        # "pihole"
+      ];
+
+      ports = [
+        "8096:8096"
+      ];
+
+      volumes = [
+        "/media/Show:/data/tvshows"
+        "/media/Movie:/data/movies"
+        "/media/TV-Music:/data/music"
+
+        "jellyfin_config:/config"
+      ];
+
+      environment = {
+        PUID = "3000";
+        PGID = "3000";
+        TZ = "Europe/Berlin";
+      };
+
+      extraOptions = [
+        # "--privileged"
+        "--device=nvidia.com/gpu=all"
+        "--net=behind-nginx"
+      ];
     };
-
-    dependsOn = [
-      # "pihole"
-    ];
-
-    ports = [
-      "8096:8096"
-    ];
-
-    volumes = [
-      "/media/Show:/data/tvshows"
-      "/media/Movie:/data/movies"
-      "/media/TV-Music:/data/music"
-
-      "jellyfin_config:/config"
-    ];
-
-    environment = {
-      PUID = "3000";
-      PGID = "3000";
-      TZ = "Europe/Berlin";
-    };
-
-    extraOptions = [
-      # "--privileged"
-      "--device=nvidia.com/gpu=all"
-      "--net=behind-nginx"
-    ];
   };
 }
