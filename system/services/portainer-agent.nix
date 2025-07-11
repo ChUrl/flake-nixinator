01bd1c250ff93ec5572b2343ea7bdb0c3e87d919
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-11 21:49:06 +0200

    Services/Portainer: Update to v2.31.3

--- system/services/portainer-agent.nix
+++ system/services/portainer-agent.nix
@@ -3,36 +3,42 @@
   lib,
   pkgs,
   ...
-}: {
-  virtualisation.oci-containers.containers.portainer-agent = {
-    image = "portainer/agent:latest";
-    autoStart = true;
+}: let
+  # Match this with the portainer-ce version
+  portainerVersion = "2.31.3";
+in {
+  # Use the agent to connect clients to a main portainer instance
+  virtualisation.oci-containers.containers = {
+    portainer-agent = {
+      image = "portainer/agent:${portainerVersion}";
+      autoStart = true;
 
-    login = {
-      # Uses DockerHub by default
-      # registry = "";
+      login = {
+        # Uses DockerHub by default
+        # registry = "";
 
-      # DockerHub Credentials
-      username = "christoph.urlacher@protonmail.com";
-      passwordFile = "${config.sops.secrets.docker-password.path}";
+        # DockerHub Credentials
+        username = "christoph.urlacher@protonmail.com";
+        passwordFile = "${config.sops.secrets.docker-password.path}";
+      };
+
+      dependsOn = [];
+
+      ports = [
+        "9001:9001"
+      ];
+
+      volumes = [
+        "/var/run/docker.sock:/var/run/docker.sock"
+        "/var/lib/docker/volumes:/var/lib/docker/volumes"
+      ];
+
+      environment = {};
+
+      extraOptions = [
+        # This container needs to be accessible from another machine inside the LAN
+        # "--net=behind-nginx"
+      ];
     };
-
-    dependsOn = [];
-
-    ports = [
-      "9001:9001"
-    ];
-
-    volumes = [
-      "/var/run/docker.sock:/var/run/docker.sock"
-      "/var/lib/docker/volumes:/var/lib/docker/volumes"
-    ];
-
-    environment = {};
-
-    extraOptions = [
-      # This container needs to be accessible from another machine inside the LAN
-      # "--net=behind-nginx"
-    ];
   };
 }
