e2a8ee5d77dd9499abca935eedb8612c667ec43d
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-11 19:32:10 +0200

    Services/Adguard: Update to v0.107.63

--- system/services/0_TEMPLATE.nix
+++ system/services/0_TEMPLATE.nix
@@ -3,45 +3,49 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  TEMPLATEVersion = "";
+in {
   # If we need to pass secrets to containers we can't use plain env variables.
   sops.templates."TEMPLATE_secrets.env".content = ''
     SECRET=${config.sops.placeholder.SECRET}
   '';
 
-  virtualisation.oci-containers.containers.TEMPLATE = {
-    image = "TEMPLATE";
-    autoStart = true;
+  virtualisation.oci-containers.containers = {
+    TEMPLATE = {
+      image = "TEMPLATE:${TEMPLATEVersion}";
+      autoStart = true;
 
-    login = {
-      # Uses DockerHub by default
-      # registry = "";
+      login = {
+        # Uses DockerHub by default
+        # registry = "";
 
-      # DockerHub Credentials
-      username = "christoph.urlacher@protonmail.com";
-      passwordFile = "${config.sops.secrets.docker-password.path}";
+        # DockerHub Credentials
+        username = "christoph.urlacher@protonmail.com";
+        passwordFile = "${config.sops.secrets.docker-password.path}";
+      };
+
+      dependsOn = [];
+
+      ports = [];
+
+      volumes = [];
+
+      environment = {
+        PUID = "1000";
+        PGID = "1000";
+        TZ = "Europe/Berlin";
+      };
+
+      environmentFiles = [
+        config.sops.templates."TEMPLATE_secrets.env".path
+      ];
+
+      extraOptions = [
+        # "--privileged"
+        # "--device=nvidia.com/gpu=all"
+        "--net=behind-nginx"
+      ];
     };
-
-    dependsOn = [];
-
-    ports = [];
-
-    volumes = [];
-
-    environment = {
-      PUID = "1000";
-      PGID = "1000";
-      TZ = "Europe/Berlin";
-    };
-
-    environmentFiles = [
-      config.sops.templates."TEMPLATE_secrets.env".path
-    ];
-
-    extraOptions = [
-      # "--privileged"
-      # "--device=nvidia.com/gpu=all"
-      "--net=behind-nginx"
-    ];
   };
 }
--- system/services/adguard.nix
+++ system/services/adguard.nix
@@ -3,56 +3,60 @@
   lib,
   pkgs,
   ...
-}: {
-  virtualisation.oci-containers.containers.adguard = {
-    image = "adguard/adguardhome";
-    autoStart = true;
+}: let
+  adguardVersion = "v0.107.63";
+in {
+  virtualisation.oci-containers.containers = {
+    adguard = {
+      image = "adguard/adguardhome:${adguardVersion}";
+      autoStart = true;
 
-    login = {
-      # Uses DockerHub by default
-      # registry = "";
+      login = {
+        # Uses DockerHub by default
+        # registry = "";
 
-      # DockerHub Credentials
-      username = "christoph.urlacher@protonmail.com";
-      passwordFile = "${config.sops.secrets.docker-password.path}";
+        # DockerHub Credentials
+        username = "christoph.urlacher@protonmail.com";
+        passwordFile = "${config.sops.secrets.docker-password.path}";
+      };
+
+      dependsOn = [];
+
+      ports = [
+        # DNS server
+        "53:53/tcp"
+        "53:53/udp"
+        # "853:853/tcp" # DNS over TLS
+        # "853:853/udp" # DNS over QUIC
+
+        # DHCP server
+        # "67:67/udp"
+        # "68:68/tcp"
+        # "68:68/udp"
+
+        # Admin panel + DNS over HTTPS
+        # "80:80/tcp"
+        # "443:443/tcp"
+        # "443:443/udp"
+        # "3100:3000/tcp" # Web interface
+
+        # DNSCrypt
+        # "5443:5443/tcp"
+        # "5443:5443/udp"
+
+        # "6060:6060/tcp" # Debugging
+      ];
+
+      volumes = [
+        "adguard_config:/opt/adguardhome/conf"
+        "adguard_work:/opt/adguardhome/work"
+      ];
+
+      environment = {};
+
+      extraOptions = [
+        "--net=behind-nginx"
+      ];
     };
-
-    dependsOn = [];
-
-    ports = [
-      # DNS server
-      "53:53/tcp"
-      "53:53/udp"
-      # "853:853/tcp" # DNS over TLS
-      # "853:853/udp" # DNS over QUIC
-
-      # DHCP server
-      # "67:67/udp"
-      # "68:68/tcp"
-      # "68:68/udp"
-
-      # Admin panel + DNS over HTTPS
-      # "80:80/tcp"
-      # "443:443/tcp"
-      # "443:443/udp"
-      # "3100:3000/tcp" # Web interface
-
-      # DNSCrypt
-      # "5443:5443/tcp"
-      # "5443:5443/udp"
-
-      # "6060:6060/tcp" # Debugging
-    ];
-
-    volumes = [
-      "adguard_config:/opt/adguardhome/conf"
-      "adguard_work:/opt/adguardhome/work"
-    ];
-
-    environment = {};
-
-    extraOptions = [
-      "--net=behind-nginx"
-    ];
   };
 }
