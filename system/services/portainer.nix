e54249d86d6aeacd39321a7c47ec8f9f47360398
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-11 21:49:06 +0200

    Services/Portainer: Update to v2.31.3

--- system/services/portainer.nix
+++ system/services/portainer.nix
@@ -3,37 +3,42 @@
   lib,
   pkgs,
   ...
-}: {
-  virtualisation.oci-containers.containers.portainer = {
-    image = "portainer/portainer-ce:latest";
-    autoStart = true;
+}: let
+  # Match this with the portainer agent version
+  portainerVersion = "2.31.3";
+in {
+  virtualisation.oci-containers.containers = {
+    portainer = {
+      image = "portainer/portainer-ce:${portainerVersion}";
+      autoStart = true;
 
-    login = {
-      # Uses DockerHub by default
-      # registry = "";
+      login = {
+        # Uses DockerHub by default
+        # registry = "";
 
-      # DockerHub Credentials
-      username = "christoph.urlacher@protonmail.com";
-      passwordFile = "${config.sops.secrets.docker-password.path}";
+        # DockerHub Credentials
+        username = "christoph.urlacher@protonmail.com";
+        passwordFile = "${config.sops.secrets.docker-password.path}";
+      };
+
+      dependsOn = [];
+
+      ports = [
+        # "8000:8000"
+        # "9443:9443"
+      ];
+
+      volumes = [
+        "portainer_config:/data"
+
+        "/var/run/docker.sock:/var/run/docker.sock"
+      ];
+
+      environment = {};
+
+      extraOptions = [
+        "--net=behind-nginx"
+      ];
     };
-
-    dependsOn = [];
-
-    ports = [
-      # "8000:8000"
-      # "9443:9443"
-    ];
-
-    volumes = [
-      "portainer_config:/data"
-
-      "/var/run/docker.sock:/var/run/docker.sock"
-    ];
-
-    environment = {};
-
-    extraOptions = [
-      "--net=behind-nginx"
-    ];
   };
 }
