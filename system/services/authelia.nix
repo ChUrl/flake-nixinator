2da56f34aa431fc24df3fa8bd2b6b0097ddd5ddd
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-11 19:57:27 +0200

    Services/Authelia: Update to v4.39.4

--- system/services/authelia.nix
+++ system/services/authelia.nix
@@ -3,38 +3,42 @@
   lib,
   pkgs,
   ...
-}: {
-  virtualisation.oci-containers.containers.authelia = {
-    image = "authelia/authelia:latest";
-    autoStart = true;
+}: let
+  autheliaVersion = "4.39.4";
+in {
+  virtualisation.oci-containers.containers = {
+    authelia = {
+      image = "authelia/authelia:${autheliaVersion}";
+      autoStart = true;
 
-    login = {
-      # Uses DockerHub by default
-      # registry = "";
+      login = {
+        # Uses DockerHub by default
+        # registry = "";
 
-      # DockerHub Credentials
-      username = "christoph.urlacher@protonmail.com";
-      passwordFile = "${config.sops.secrets.docker-password.path}";
+        # DockerHub Credentials
+        username = "christoph.urlacher@protonmail.com";
+        passwordFile = "${config.sops.secrets.docker-password.path}";
+      };
+
+      dependsOn = [
+        # "pihole"
+      ];
+
+      ports = [
+        # "9091:9091"
+      ];
+
+      volumes = [
+        "authelia_config:/config"
+      ];
+
+      environment = {
+        TZ = "Europe/Berlin";
+      };
+
+      extraOptions = [
+        "--net=behind-nginx"
+      ];
     };
-
-    dependsOn = [
-      # "pihole"
-    ];
-
-    ports = [
-      # "9091:9091"
-    ];
-
-    volumes = [
-      "authelia_config:/config"
-    ];
-
-    environment = {
-      TZ = "Europe/Berlin";
-    };
-
-    extraOptions = [
-      "--net=behind-nginx"
-    ];
   };
 }
