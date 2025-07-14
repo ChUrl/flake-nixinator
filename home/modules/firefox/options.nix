7120de42672cca2cf64fae76d61f7113f7a96ef1
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-15 00:53:48 +0200

    Modules/Firefox: Remove obsolete options

--- home/modules/firefox/options.nix
+++ home/modules/firefox/options.nix
@@ -9,6 +9,4 @@ with mylib.modules; {
   wayland = mkBoolOption false "Enable firefox wayland support";
   vaapi = mkBoolOption false "Enable firefox vaapi support";
   disableTabBar = mkBoolOption false "Disable the firefox tab bar (for TST)";
-  defaultBookmarks = mkBoolOption false "Preset standard bookmarks and folders";
-  gnomeTheme = mkBoolOption false "Use Firefox gnome theme (rafaelmardojai)";
 }
