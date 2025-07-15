7a3df8a5ec5188b06398e82cf6b4daa2ea958ad9
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-15 21:30:52 +0200

    Flake: Add impermanence input

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -27,8 +27,6 @@
     # HM modules imported from the flake inputs
     inputs.nix-flatpak.homeManagerModules.nix-flatpak
     inputs.nixvim.homeManagerModules.nixvim
-    # inputs.agenix.homeManagerModules.default
-    # inputs.ags.homeManagerModules.default
-    # inputs.spicetify-nix.homeManagerModules.default
+    inputs.impermanence.homeManagerModules.impermanence
   ];
 }
