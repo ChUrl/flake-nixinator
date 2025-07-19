00a299ba40cbd02328404f8c31e9ca7192acf2da
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-19 15:47:30 +0200

    Modules/Kitty: Switch to dark colorscheme

--- home/modules/kitty/default.nix
+++ home/modules/kitty/default.nix
@@ -5,7 +5,7 @@
   mylib,
   ...
 }: let
-  inherit (config.modules) kitty color;
+  inherit (config.modules) kitty;
 in {
   options.modules.kitty = import ./options.nix {inherit lib mylib;};
 
@@ -15,7 +15,7 @@ in {
       shellIntegration.enableFishIntegration = true;
 
       font = {
-        name = "${color.font}";
+        name = "${config.modules.color.font}";
         size = 12;
       };
 
@@ -28,8 +28,7 @@ in {
       };
 
       settings = let
-        light = color.hex.light;
-        dark = color.hex.dark;
+        color = config.modules.color.hex.dark;
       in {
         editor = config.home.sessionVariables.EDITOR;
         scrollback_lines = 10000;
@@ -37,10 +36,10 @@ in {
         # hide_window_decorations = "yes";
         enabled_layouts = "grid,vertical,horizontal";
 
-        allow_remote_control = "yes"; # For nnn file preview
+        allow_remote_control = "yes"; # For nnn file preview or nvim scrollback
         listen_on = "unix:@mykitty";
 
-        tab_bar_min_tabs = 1;
+        tab_bar_min_tabs = 2; # Don't show a single tab
         tab_bar_edge = "bottom";
         tab_bar_style = "powerline";
         tab_powerline_style = "round";
@@ -51,74 +50,74 @@ in {
         #
 
         # The basic colors
-        background = "#${light.base}";
-        foreground = "#${light.text}";
-        selection_foreground = "#${light.base}";
-        selection_background = "#${light.rosewater}";
+        background = "#${color.base}";
+        foreground = "#${color.text}";
+        selection_foreground = "#${color.base}";
+        selection_background = "#${color.rosewater}";
 
         # Cursor colors
-        cursor = "#${light.rosewater}";
-        cursor_text_color = "#${light.base}";
+        cursor = "#${color.rosewater}";
+        cursor_text_color = "#${color.base}";
 
         # URL underline color when hovering with mouse
-        url_color = "#${light.rosewater}";
+        url_color = "#${color.rosewater}";
 
         # Kitty window border colors
-        active_border_color = "#${light.lavender}";
-        inactive_border_color = "#${light.overlay0}";
-        bell_border_color = "#${light.yellow}";
+        active_border_color = "#${color.lavender}";
+        inactive_border_color = "#${color.overlay0}";
+        bell_border_color = "#${color.yellow}";
 
         # OS Window titlebar colors
         wayland_titlebar_color = "system";
         macos_titlebar_color = "system";
 
         # Tab bar colors
-        active_tab_foreground = "#${dark.base}";
-        active_tab_background = "#${dark.lavender}";
-        inactive_tab_foreground = "#${dark.text}";
-        inactive_tab_background = "#${dark.crust}";
-        tab_bar_background = "#${light.base}";
+        active_tab_foreground = "#${color.base}";
+        active_tab_background = "#${color.lavender}";
+        inactive_tab_foreground = "#${color.text}";
+        inactive_tab_background = "#${color.crust}";
+        tab_bar_background = "#${color.base}";
 
         # Color for marks (marked text in the terminal)
-        mark1_foreground = "#${light.base}";
-        mark1_background = "#${light.lavender}";
-        mark2_foreground = "#${light.base}";
-        mark2_background = "#${light.mauve}";
-        mark3_foreground = "#${light.base}";
-        mark3_background = "#${light.sapphire}";
+        mark1_foreground = "#${color.base}";
+        mark1_background = "#${color.lavender}";
+        mark2_foreground = "#${color.base}";
+        mark2_background = "#${color.mauve}";
+        mark3_foreground = "#${color.base}";
+        mark3_background = "#${color.sapphire}";
 
         # The 16 terminal colors
         # black
-        color0 = "#${light.subtext1}";
-        color8 = "#${light.subtext0}";
+        color0 = "#${color.subtext1}";
+        color8 = "#${color.subtext0}";
 
         # red
-        color1 = "#${light.red}";
-        color9 = "#${light.red}";
+        color1 = "#${color.red}";
+        color9 = "#${color.red}";
 
         # green
-        color2 = "#${light.green}";
-        color10 = "#${light.green}";
+        color2 = "#${color.green}";
+        color10 = "#${color.green}";
 
         # yellow
-        color3 = "#${light.yellow}";
-        color11 = "#${light.yellow}";
+        color3 = "#${color.yellow}";
+        color11 = "#${color.yellow}";
 
         # blue
-        color4 = "#${light.blue}";
-        color12 = "#${light.blue}";
+        color4 = "#${color.blue}";
+        color12 = "#${color.blue}";
 
         # magenta
-        color5 = "#${light.pink}";
-        color13 = "#${light.pink}";
+        color5 = "#${color.pink}";
+        color13 = "#${color.pink}";
 
         # cyan
-        color6 = "#${light.teal}";
-        color14 = "#${light.teal}";
+        color6 = "#${color.teal}";
+        color14 = "#${color.teal}";
 
         # white
-        color7 = "#${light.surface2}";
-        color15 = "#${light.surface1}";
+        color7 = "#${color.surface2}";
+        color15 = "#${color.surface1}";
       };
     };
   };
