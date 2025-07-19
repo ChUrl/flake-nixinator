20af4590b01f2f5b35904d3ee7138b4cc5b495fb
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-20 00:42:37 +0200

    Modules/Rofi: Remove obsolete theme option

--- home/modules/rofi/options.nix
+++ home/modules/rofi/options.nix
@@ -6,10 +6,4 @@
 with lib;
 with mylib.modules; {
   enable = mkEnableOption "Rofi";
-
-  theme = mkOption {
-    type = types.str;
-    example = "Three-Bears";
-    description = "Color theme to use";
-  };
 }
