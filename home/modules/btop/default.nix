6c61c30294b441983eed56a80120455c827a0259
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-07-20 15:22:20 +0200

    Modules/Botp: Use updated color module for theme

--- home/modules/btop/default.nix
+++ home/modules/btop/default.nix
@@ -6,7 +6,7 @@
   pkgs,
   ...
 }: let
-  inherit (config.modules) btop;
+  inherit (config.modules) btop color;
 in {
   options.modules.btop = import ./options.nix {inherit lib mylib;};
 
@@ -18,98 +18,97 @@ in {
         then pkgs.btop-cuda
         else pkgs.btop;
 
-      # TODO: Use colors module
       themes = {
         # https://github.com/catppuccin/btop/tree/main
-        catppuccin-latte = ''
+        system-theme = ''
           # Main background, empty for terminal default, need to be empty if you want transparent background
-          theme[main_bg]="#eff1f5"
+          theme[main_bg]=${color.hexS.base}
 
           # Main text color
-          theme[main_fg]="#4c4f69"
+          theme[main_fg]=${color.hexS.text}
 
           # Title color for boxes
-          theme[title]="#4c4f69"
+          theme[title]=${color.hexS.text}
 
           # Highlight color for keyboard shortcuts
-          theme[hi_fg]="#1e66f5"
+          theme[hi_fg]=${color.hexS.blue}
 
           # Background color of selected item in processes box
-          theme[selected_bg]="#bcc0cc"
+          theme[selected_bg]=${color.hexS.surface1}
 
           # Foreground color of selected item in processes box
-          theme[selected_fg]="#1e66f5"
+          theme[selected_fg]=${color.hexS.blue}
 
           # Color of inactive/disabled text
-          theme[inactive_fg]="#8c8fa1"
+          theme[inactive_fg]=${color.hexS.overlay1}
 
           # Color of text appearing on top of graphs, i.e uptime and current network graph scaling
-          theme[graph_text]="#dc8a78"
+          theme[graph_text]=${color.hexS.rosewater}
 
           # Background color of the percentage meters
-          theme[meter_bg]="#bcc0cc"
+          theme[meter_bg]=${color.hexS.surface1}
 
           # Misc colors for processes box including mini cpu graphs, details memory graph and details status text
-          theme[proc_misc]="#dc8a78"
+          theme[proc_misc]=${color.hexS.rosewater}
 
           # CPU, Memory, Network, Proc box outline colors
-          theme[cpu_box]="#8839ef" #Mauve
-          theme[mem_box]="#40a02b" #Green
-          theme[net_box]="#e64553" #Maroon
-          theme[proc_box]="#1e66f5" #Blue
+          theme[cpu_box]=${color.hexS.mauve} #Mauve
+          theme[mem_box]=${color.hexS.green} #Green
+          theme[net_box]=${color.hexS.maroon} #Maroon
+          theme[proc_box]=${color.hexS.blue} #Blue
 
           # Box divider line and small boxes line color
-          theme[div_line]="#9ca0b0"
+          theme[div_line]=${color.hexS.overlay0}
 
           # Temperature graph color (Green -> Yellow -> Red)
-          theme[temp_start]="#40a02b"
-          theme[temp_mid]="#df8e1d"
-          theme[temp_end]="#d20f39"
+          theme[temp_start]=${color.hexS.green}
+          theme[temp_mid]=${color.hexS.yellow}
+          theme[temp_end]=${color.hexS.red}
 
           # CPU graph colors (Teal -> Lavender)
-          theme[cpu_start]="#179299"
-          theme[cpu_mid]="#209fb5"
-          theme[cpu_end]="#7287fd"
+          theme[cpu_start]=${color.hexS.teal}
+          theme[cpu_mid]=${color.hexS.sapphire}
+          theme[cpu_end]=${color.hexS.lavender}
 
           # Mem/Disk free meter (Mauve -> Lavender -> Blue)
-          theme[free_start]="#8839ef"
-          theme[free_mid]="#7287fd"
-          theme[free_end]="#1e66f5"
+          theme[free_start]=${color.hexS.mauve}
+          theme[free_mid]=${color.hexS.lavender}
+          theme[free_end]=${color.hexS.blue}
 
           # Mem/Disk cached meter (Sapphire -> Lavender)
-          theme[cached_start]="#209fb5"
-          theme[cached_mid]="#1e66f5"
-          theme[cached_end]="#7287fd"
+          theme[cached_start]=${color.hexS.sapphire}
+          theme[cached_mid]=${color.hexS.blue}
+          theme[cached_end]=${color.hexS.lavender}
 
           # Mem/Disk available meter (Peach -> Red)
-          theme[available_start]="#fe640b"
-          theme[available_mid]="#e64553"
-          theme[available_end]="#d20f39"
+          theme[available_start]=${color.hexS.peach}
+          theme[available_mid]=${color.hexS.maroon}
+          theme[available_end]=${color.hexS.red}
 
           # Mem/Disk used meter (Green -> Sky)
-          theme[used_start]="#40a02b"
-          theme[used_mid]="#179299"
-          theme[used_end]="#04a5e5"
+          theme[used_start]=${color.hexS.green}
+          theme[used_mid]=${color.hexS.teal}
+          theme[used_end]=${color.hexS.sky}
 
           # Download graph colors (Peach -> Red)
-          theme[download_start]="#fe640b"
-          theme[download_mid]="#e64553"
-          theme[download_end]="#d20f39"
+          theme[download_start]=${color.hexS.peach}
+          theme[download_mid]=${color.hexS.maroon}
+          theme[download_end]=${color.hexS.red}
 
           # Upload graph colors (Green -> Sky)
-          theme[upload_start]="#40a02b"
-          theme[upload_mid]="#179299"
-          theme[upload_end]="#04a5e5"
+          theme[upload_start]=${color.hexS.green}
+          theme[upload_mid]=${color.hexS.teal}
+          theme[upload_end]=${color.hexS.sky}
 
           # Process box color gradient for threads, mem and cpu usage (Sapphire -> Mauve)
-          theme[process_start]="#209fb5"
-          theme[process_mid]="#7287fd"
-          theme[process_end]="#8839ef"
+          theme[process_start]=${color.hexS.sapphire}
+          theme[process_mid]=${color.hexS.lavender}
+          theme[process_end]=${color.hexS.mauve}
         '';
       };
 
       settings = {
-        color_theme = "catppuccin-latte";
+        color_theme = "system-theme";
         truecolor = true;
         vim_keys = true;
         rounded_corners = true;
