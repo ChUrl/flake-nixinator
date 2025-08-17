6167efb4c1dfd54031ec2ebdec6765289be9ab3c
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-08-17 17:56:16 +0200

    Overlays: Disable neovide overlay (0.15.1 hit nixpkgs)

--- overlays/default.nix
+++ overlays/default.nix
@@ -26,24 +26,23 @@
     #     localSystem = {inherit (prev) system;};
     #   }).unityhub;
 
-    # TODO: Remove this after 0.15.1 hits nixpkgs
-    neovide = prev.neovide.overrideAttrs (finalAttrs: prevAttrs: {
-      version = "0.15.1";
-      src = prev.fetchFromGitHub {
-        owner = "neovide";
-        repo = "neovide";
-        tag = finalAttrs.version;
-        hash = "sha256-2iV3g6tcCkMF7sFG/GZDz3czPZNIDi6YLfrVzYO9jYI=";
-      };
-      cargoHash = "sha256-YlHAcUCRk6ROg5yXIumHfsiR/2TrsSzbuXz/IQK7sEo=";
-      cargoDeps = prev.rustPlatform.fetchCargoVendor {
-        inherit (finalAttrs) pname src version;
-        hash = finalAttrs.cargoHash;
-      };
-    });
+    # neovide = prev.neovide.overrideAttrs (finalAttrs: prevAttrs: {
+    #   version = "0.15.1";
+    #   src = prev.fetchFromGitHub {
+    #     owner = "neovide";
+    #     repo = "neovide";
+    #     tag = finalAttrs.version;
+    #     hash = "sha256-2iV3g6tcCkMF7sFG/GZDz3czPZNIDi6YLfrVzYO9jYI=";
+    #   };
+    #   cargoHash = "sha256-YlHAcUCRk6ROg5yXIumHfsiR/2TrsSzbuXz/IQK7sEo=";
+    #   cargoDeps = prev.rustPlatform.fetchCargoVendor {
+    #     inherit (finalAttrs) pname src version;
+    #     hash = finalAttrs.cargoHash;
+    #   };
+    # });
 
-    # TODO: Remove this after jetbrains.jdk builds again (nixpkgs issue 425328)
-    jetbrains.rider = pkgs-stable.jetbrains.rider;
+    # Remove this after jetbrains.jdk builds again (nixpkgs issue 425328)
+    # jetbrains.rider = pkgs-stable.jetbrains.rider;
   };
 in
   # Composes a list of overlays and returns a single overlay function that combines them.
