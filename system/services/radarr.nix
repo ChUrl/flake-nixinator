829f959ce315507d0a858765959d430928eabc83
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-11-15 01:07:31 +0100

    Services/Radarr: Init at v5.28.0

---
+++ system/services/radarr.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  radarrVersion = "5.28.0";
+in {
+  virtualisation.oci-containers.containers = {
+    radarr = {
+      image = "linuxserver/radarr:${radarrVersion}";
+      autoStart = true;
+
+      login = {
+        # Uses DockerHub by default
+        # registry = "";
+
+        # DockerHub Credentials
+        username = "christoph.urlacher@protonmail.com";
+        passwordFile = "${config.sops.secrets.docker-password.path}";
+      };
+
+      dependsOn = [];
+
+      ports = [
+        # "7878:7878"
+      ];
+
+      volumes = [
+        "/media/Movie/0-MakeMKV:/media/downloads"
+        "/media/Movie:/media/movies"
+
+        "radarr_config:/config"
+      ];
+
+      environment = {
+        PUID = "3000";
+        PGID = "3000";
+        TZ = "Europe/Berlin";
+      };
+
+      extraOptions = [
+        # "--privileged"
+        # "--device=nvidia.com/gpu=all"
+        "--net=behind-nginx"
+      ];
+    };
+  };
+}
