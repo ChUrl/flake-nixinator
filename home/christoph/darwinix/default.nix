50d14ecf83d7030f12206d1cfaef12ee5d610c12
Christoph Urlacher <christoph.urlacher@protonmail.com>
2026-03-21 15:03:29 +0100

    Home/Darwinix: Remove non-available packages

--- home/christoph/darwinix/default.nix
+++ home/christoph/darwinix/default.nix
@@ -131,40 +131,6 @@
         };
       };
 
-      cava = {
-        enable = true;
-
-        settings = {
-          general = {
-            framerate = 60; # default 60
-            autosens = 1; # default 1
-            sensitivity = 100; # default 100
-            lower_cutoff_freq = 50; # not passed to cava if not provided
-            higher_cutoff_freq = 10000; # not passed to cava if not provided
-          };
-
-          smoothing = {
-            noise_reduction = 77; # default 77
-            monstercat = false; # default false
-            waves = false; # default false
-          };
-
-          color = {
-            # https://github.com/catppuccin/cava/blob/main/themes/latte-transparent.cava
-            gradient = 1;
-
-            gradient_color_1 = "'${color.hexS.teal}'";
-            gradient_color_2 = "'${color.hexS.sky}'";
-            gradient_color_3 = "'${color.hexS.sapphire}'";
-            gradient_color_4 = "'${color.hexS.blue}'";
-            gradient_color_5 = "'${color.hexS.mauve}'";
-            gradient_color_6 = "'${color.hexS.pink}'";
-            gradient_color_7 = "'${color.hexS.maroon}'";
-            gradient_color_8 = "'${color.hexS.red}'";
-          };
-        };
-      };
-
       direnv = {
         enable = true;
         nix-direnv.enable = true;
