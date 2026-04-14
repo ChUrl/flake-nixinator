7e3985bd348e6f98ab76098a029c9f1022d49695
Christoph Urlacher <christoph.urlacher@protonmail.com>
2026-04-14 11:05:49 +0200

    Services/Comfyui: Disable service

--- system/services/comfyui.nix
+++ system/services/comfyui.nix
@@ -6,7 +6,9 @@
   ...
 }: let
   # comfyuiVersion = "cu128-slim-20260316";
-  comfyuiVersion = "cu128-megapak-20260323";
+  # comfyuiVersion = "cu128-megapak-20260413";
+  # comfyuiVersion = "cu130-megapak-pt211-20260330";
+  comfyuiVersion = "cu130-megapak-pt211-20260413";
 in {
   virtualisation.oci-containers.containers = {
     comfyui = {
@@ -24,13 +26,28 @@ in {
       volumes = let
         rootDir = "/home/christoph/Games/ComfyUI";
       in [
-        "${rootDir}/storage:/root"
+        # cu128-slim / cu128-megapak
+        # "${rootDir}/storage:/root"
+        # "${rootDir}/storage-models/models:/root/ComfyUI/models"
+        # "${rootDir}/storage-models/hf-hub:/root/.cache/huggingface/hub"
+        # "${rootDir}/storage-models/torch-hub:/root/.cache/torch/hub"
+        # "${rootDir}/storage-user/input:/root/ComfyUI/input"
+        # "${rootDir}/storage-user/output:/root/ComfyUI/output"
+        # "${rootDir}/storage-user/workflows:/root/ComfyUI/user/default/workflows"
+
+        # cu130-megapak
+        "${rootDir}/storage-cache/dot-cache:/root/.cache"
+        "${rootDir}/storage-cache/dot-config:/root/.config"
+        "${rootDir}/storage-nodes/dot-local:/root/.local"
+        "${rootDir}/storage-nodes/comfy-extras:/root/ComfyUI/comfy_extras"
+        "${rootDir}/storage-nodes/custom_nodes:/root/ComfyUI/custom_nodes"
         "${rootDir}/storage-models/models:/root/ComfyUI/models"
         "${rootDir}/storage-models/hf-hub:/root/.cache/huggingface/hub"
         "${rootDir}/storage-models/torch-hub:/root/.cache/torch/hub"
         "${rootDir}/storage-user/input:/root/ComfyUI/input"
         "${rootDir}/storage-user/output:/root/ComfyUI/output"
-        "${rootDir}/storage-user/workflows:/root/ComfyUI/user/default/workflows"
+        "${rootDir}/storage-user/user-profile:/root/ComfyUI/user"
+        "${rootDir}/storage-user/user-scripts:/root/user-scripts"
       ];
 
       environment = {
@@ -39,12 +56,15 @@ in {
         TZ = "Europe/Berlin";
 
         # https://github.com/Comfy-Org/ComfyUI/blob/master/comfy/cli_args.py
-        # --use-sage-attention # => OOM
-        # --lowvram
-        # --disable-pinned-memory
-        # --cache-none
-        # --reserve-vram 1 # (1 or 2) => Assume less vram is available to mitigate OOM due to wrong vram estimation
-        # CLI_ARGS = "--lowvram --disable-pinned-memory --cache-none --reserve-vram 2";
+        CLI_ARGS = lib.concatStringsSep " " [
+          # "--cache-none" # Leads to single nodes being executed multiple times for each output connection :/
+          "--lowvram"
+          "--disable-smart-memory"
+          "--disable-pinned-memory"
+          "--disable-xformers"
+          "--use-sage-attention" # Crashes
+          # "--reserve-vram 1" # (1 or 2) => Assume less vram is available to mitigate OOM due to wrong vram estimation
+        ];
       };
 
       extraOptions = [
