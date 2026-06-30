d9b0685aa493d15732ae706f446fdf62dfe953e1
Christoph Urlacher <christoph.urlacher@protonmail.com>
2026-07-01 01:51:45 +0200

    Modules/Neovim: Update latex snippets

--- home/homemodules/neovim/snippets_latex.lua
+++ home/homemodules/neovim/snippets_latex.lua
@@ -1,3 +1,18 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local extras = require("luasnip.extras")
+local rep = extras.rep
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+
 return {
 	-- The first list contains manually expanded snippts
 	--
@@ -18,7 +33,7 @@ return {
 
 	-- \begin{environment}
 	s(
-		"beg",
+		{ trig = "beg", name = "begin/end", descr = "begin/end environment (generic)" },
 		fmta(
 			[[
 		        \begin{<>}
@@ -26,6 +41,28 @@ return {
 		        \end{<>}
 	        ]],
 			{ i(1), i(0), rep(1) }
+		)
+	),
+	s(
+		{ trig = "item", name = "itemize", dscr = "bullet points (itemize)" },
+		fmta(
+			[[
+				\begin{itemize}
+				\item <>
+				\end{itemize}
+			]],
+			{ i(0) }
+		)
+	),
+	s(
+		{ trig = "enum", name = "enumerate", dscr = "bullet points (enumerate)" },
+		fmta(
+			[[
+				\begin{enumerate}
+				\item <>
+				\end{enumerate}
+			]],
+			{ i(0) }
 		)
 	),
 }, {
