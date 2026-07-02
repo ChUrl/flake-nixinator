bba0c76ce29ce8f4fe107f9bd9adb63548d074cf
Christoph Urlacher <christoph.urlacher@protonmail.com>
2026-07-03 00:54:46 +0200

    Modules/Neovim: Update vimtex config

--- home/homemodules/neovim/default.nix
+++ home/homemodules/neovim/default.nix
@@ -523,7 +523,7 @@ in {
               };
 
               sources = {
-                default = ["lsp" "path" "snippets"]; # No "buffer"
+                default = ["lsp" "path" "snippets" "omni"]; # No "buffer"
               };
 
               cmdline = {
@@ -2149,7 +2149,23 @@ in {
               function()
                 vim.g.vimtex_mappings_enabled = true
                 vim.g.vimtex_view_method = "zathura"
-                vim.g.vimtex_syntax_conceal_disable = true;
+                vim.g.vimtex_syntax_conceal_disable = false
+                vim.g.vimtex_syntax_conceal = {
+                  accents = 1,
+                  ligatures = 1,
+                  cites = 1,
+                  fancy = 1,
+                  texTabularChar = 1,
+                  spacing = 1,
+                  greek = 1,
+                  math_bounds = 1,
+                  math_delimiters = 1,
+                  math_fracs = 1,
+                  math_super_sub = 1,
+                  math_symbols = 1,
+                  sections = 1,
+                  styles = 1,
+                }
 
                 vim.g.vimtex_compiler_method = "latexmk"
                 vim.g.vimtex_compiler_clean_paths = {"_minted*", "svg-inkscape*"}
