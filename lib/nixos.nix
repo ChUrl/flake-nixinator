305995f5fd025c1f6c0586a13f1a7de94a7c39af
Christoph Urlacher <christoph.urlacher@protonmail.com>
2026-07-28 21:01:32 +0200

    Lib/Nixos: Add nix-gaming to substituters

--- lib/nixos.nix
+++ lib/nixos.nix
@@ -25,12 +25,14 @@
         "https://nix-community.cachix.org"
         # "https://app.cachix.org/cache/nixos-rocm"
         "https://hyprland.cachix.org"
+        "https://nix-gaming.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         # "nixos-rocm.cachix.org-1:VEpsf7pRIijjd8csKjFNBGzkBqOmw8H9PRmgAq14LnE="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       ];
     };
 
